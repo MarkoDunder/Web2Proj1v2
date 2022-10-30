8582c7c07f484eefe35823ed11be79eca609bc86
MarkoDunder <marko.dundjer14@gmail.com>
2022-10-30 18:06:32 +0100

    database config2

--- backend/db/pool-Index.js
+++ backend/db/pool-Index.js
@@ -4,7 +4,7 @@ const pool= new Pool({
     user:"project1_db_user",
     password:"WMzR1wnfLLT8lBo19KHXw6FfuqpYgWZu",
     database:"project1_db",
-    host:"dpg-cdf9dv9gp3juhhupsso0-a",
+    host:"postgres://project1_db_user:WMzR1wnfLLT8lBo19KHXw6FfuqpYgWZu@dpg-cdf9dv9gp3juhhupsso0-a/project1_db",
     port:5432,
     ssl:true
 });
